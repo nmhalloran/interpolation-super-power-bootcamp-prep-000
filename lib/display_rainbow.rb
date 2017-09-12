868426e2c657cc924b3f3e55c2e0f4c8deb8dc23
Nicholas Halloran <nmhalloran@hotmail.com>
2017-09-12 03:59:31 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,7 +4,7 @@ stefani = ["Stefani", 49, "Firefighter", "123"]
 
 def print_business_card(person)
   puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
-  puts "Contact: #{{person[3]}"
+  puts "Contact: #{person[3]}"
 end
 
 print_business_card(bob)
